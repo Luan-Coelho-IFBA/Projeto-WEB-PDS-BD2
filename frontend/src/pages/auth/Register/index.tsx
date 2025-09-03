c8cbcd38e526a3d7012930f301f44b5992af7785
xOmilk <leite46052@gmail.com>
2025-09-02 22:24:39 -0300

    Pagina de cadastro

--- frontend/src/pages/auth/Register/index.tsx
+++ frontend/src/pages/auth/Register/index.tsx
@@ -1,11 +1,62 @@
+import { Form } from "../../../components/Form";
 import { RouterLink } from "../../../components/RouterLink";
-import { RoutesName } from "../../../components/routes/RoutesName";
+import { RoutesName } from "../../../constants/RoutesName";
+
+import styles from "./styles.module.css";
 
 export function Register() {
 	return (
-		<>
-			<h3>Registro</h3>
-			<RouterLink children="Ir para login" href={RoutesName.login} />
-		</>
+		<div className={styles.containerRegister}>
+			<Form>
+				<Form.Header
+					title="Cadastro"
+					subtitle="Digite suas informações para efetuar o cadastro"
+				/>
+
+				<Form.Field
+					label="NOME COMPLETO"
+					nameInput="NomeCompleto"
+					placeholder="Informe o seu nome completo"
+					type="text"
+				/>
+
+				<Form.Field
+					label="EMAIL"
+					nameInput="email"
+					placeholder="Digite um email válido"
+					type="email"
+				/>
+
+				<Form.Field
+					label="SENHA"
+					nameInput="senha"
+					placeholder="Digite sua senha"
+					type="password"
+				/>
+				<Form.Field
+					label="CONFIRMAR SENHA"
+					nameInput="confirmarSenha"
+					placeholder="Digite sua senha novamente"
+					type="email"
+				/>
+				<Form.Field
+					label="CONTATO"
+					nameInput="contato"
+					placeholder="(11) 9 1111-1111"
+					type="email"
+				/>
+
+				<Form.Button
+					onClick={() => console.log("teste")}
+					nameButton="CADASTRAR"
+				/>
+			</Form>
+			<div className={styles.links}>
+				<RouterLink
+					children="Já possui conta? Ir para login"
+					href={RoutesName.login}
+				/>
+			</div>
+		</div>
 	);
 }
