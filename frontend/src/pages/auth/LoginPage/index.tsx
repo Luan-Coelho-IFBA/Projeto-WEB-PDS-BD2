4f7a982f0e5fec4c712acbe02c0ba8418401d52e
xOmilk <leite46052@gmail.com>
2025-09-06 19:58:43 -0300

    Implement login functionality with form validation and error handling

--- frontend/src/pages/auth/LoginPage/index.tsx
+++ frontend/src/pages/auth/LoginPage/index.tsx
@@ -1,31 +1,88 @@
 import { Form } from "../../../components/Form";
 import styles from "./styles.module.css";
-import { loginAccount } from "../../../utils/loginAccount";
 import { RouterLink } from "../../../components/RouterLink";
 import { RoutesName } from "../../../constants/RoutesName";
 
+import { loginUser } from "../../../services/loginUser";
+
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import type { AxiosError } from "axios";
+import type { ApiErrorResponse } from "../../../server/types";
+
+const LoginSchema = z.object({
+	email: z
+		.email("Informe um e-email válido")
+		.min(1, "O e-mail é obrigatorio para efetuar o login"),
+	password: z
+		.string()
+		.min(1, "Este campo é obrigatório")
+		.max(30, "A senha não pode ultrapassar 30 digitos"),
+});
+
+type LoginUserForm = z.infer<typeof LoginSchema>;
+
 export function LoginPage() {
+	const {
+		setError,
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm({ resolver: zodResolver(LoginSchema) });
+
+	const onSubmitForm: SubmitHandler<LoginUserForm> = async (
+		data: LoginUserForm
+	) => {
+		try {
+			const response = await loginUser(data);
+			console.log(response);
+		} catch (error) {
+			const axiosError = error as AxiosError<ApiErrorResponse>;
+			setError("root", {
+				message: axiosError.response?.data.message,
+			});
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className={styles.containerLogin}>
-			<Form>
+			<Form onSubmit={handleSubmit(onSubmitForm)}>
 				<Form.Header
 					title="Login"
 					subtitle="Faça o login na sua conta"
 				/>
-				<Form.Field
-					nameInput=""
-					label="EMAIL"
-					placeholder="Informe o seu email"
-					type="email"
-				/>
-				<Form.Field
-					nameInput="password"
-					label="SENHA"
-					placeholder="Digite a sua senha"
-					type="password"
-				/>
-
-				<Form.Button onClick={loginAccount} nameButton="Logar" />
+				<div className={styles.group}>
+					<Form.Field
+						{...register("email")}
+						nameInput="email"
+						label="EMAIL"
+						placeholder="Informe o seu email"
+						type="text"
+					/>
+					{errors.email?.message && (
+						<p className={styles.error}>{errors.email.message}</p>
+					)}
+				</div>
+				<div className={styles.group}>
+					<Form.Field
+						{...register("password")}
+						nameInput="password"
+						label="SENHA"
+						placeholder="Digite a sua senha"
+						type="password"
+					/>
+					{errors.password?.message && (
+						<p className={styles.error}>
+							{errors.password.message}
+						</p>
+					)}
+				</div>
+				{errors.root?.message && (
+					<p className={styles.error}>{errors.root.message}</p>
+				)}
+				<Form.Button type="submit" nameButton="Logar" />
 			</Form>
 			<div className={styles.links}>
 				<RouterLink href={RoutesName.forgotPassword}>
