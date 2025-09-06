6fa993a64bc3a398d955539f8fee5cc395a34e05
xOmilk <leite46052@gmail.com>
2025-09-06 19:58:58 -0300

    Add loginUser function to handle user authentication

---
+++ frontend/src/services/loginUser.ts
@@ -0,0 +1,23 @@
+import api from "../server/api";
+import { apiRoutes } from "../server/apiRoutes";
+
+type LoginUserProps = {
+	email: string;
+	password: string;
+};
+
+export async function loginUser({ email, password }: LoginUserProps) {
+	const response = await api.post(
+		apiRoutes.auth.login,
+		{
+			email,
+			password,
+		},
+		{
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
+	return response;
+}
