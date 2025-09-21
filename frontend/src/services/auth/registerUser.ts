b8ac5f7f126130d9964d22546ebd2220522c54ff
xOmilk <leite46052@gmail.com>
2025-09-21 19:17:51 -0300

    Refactor registerUsers function to use apiRoutes for registration endpoint

--- frontend/src/services/auth/registerUser.ts
+++ frontend/src/services/auth/registerUser.ts
@@ -1,17 +1,17 @@
-import { PageRoutesName } from "../../constants/PageRoutesName";
 import api from "../../server/api";
+import { apiRoutes } from "../../server/apiRoutes";
 
 type bodyPostRegister = {
-	name: string;
-	email: string;
-	password: string;
+    name: string;
+    email: string;
+    password: string;
 };
 
 export async function registerUsers(data: bodyPostRegister) {
-	const response = await api.post(PageRoutesName.auth.register, {
-		name: data.name,
-		email: data.email,
-		password: data.password,
-	});
-	return response;
+    const response = await api.post(apiRoutes.auth.register, {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+    });
+    return response;
 }
