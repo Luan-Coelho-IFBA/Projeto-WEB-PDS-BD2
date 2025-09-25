a3db3e98a625655ccf98756818174ba31ed8a5c6
Luan-Coelho-IFBA <20231bsifsa0001@ifba.edu.br>
2025-09-25 09:59:21 -0300

    AWS

--- frontend/src/server/api.ts
+++ frontend/src/server/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://18.230.122.223" });
+const api = axios.create({ baseURL: "http://18.231.94.165:3000" });
 
 export default api;
