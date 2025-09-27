0425db7eba018178b0b939d501ffb236522d29bb
xOmilk <leite46052@gmail.com>
2025-09-26 21:55:40 -0300

    Add react-hot-toast for enhanced notification handling and create ToastInfo component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "./routes/AppRouter";
 import "./css/global.css";
 import "./css/theme.css";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 export function App() {
-	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<AppRouter />
-			</QueryClientProvider>
-		</>
-	);
+    return (
+        <>
+            <Toaster />
+            <QueryClientProvider client={queryClient}>
+                <AppRouter />
+            </QueryClientProvider>
+        </>
+    );
 }
