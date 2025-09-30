ebe7e854488bb877dcee1a0738be9809f6e58b28
xOmilk <leite46052@gmail.com>
2025-09-29 23:16:11 -0300

    Implement remove writer functionality with loading state and error handling

--- frontend/src/pages/admin/ManageWritersPage/RemoveWriterModal.tsx
+++ frontend/src/pages/admin/ManageWritersPage/RemoveWriterModal.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import { Modal } from "../../../components/Modal";
 import { ApiResponseUser } from "../../../services/auth/getWriters";
+import { changeToReader } from "../../../services/role/changeToReader";
+import { AxiosError } from "axios";
+import { ApiErrorResponse } from "../../../server/types";
+import { Loader } from "../../../components/Loader";
 
 type RemoveWriterModalProps = {
     selectedWriter: ApiResponseUser;
@@ -14,6 +19,22 @@ export function RemoveWriterModal({
     isOpen: modalRemoveWriter,
     refetchWriters,
 }: RemoveWriterModalProps) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isError, setIsError] = useState<string | undefined>();
+
+    async function removeWriterFn() {
+        setIsLoading(true);
+        try {
+            await changeToReader(selectedWriter.id);
+            setIsLoading(false);
+            refetchWriters();
+            setModalRemoveWriter(false);
+        } catch (error) {
+            const axiosError = error as AxiosError<ApiErrorResponse>;
+            setIsError(axiosError.response?.data.message);
+        }
+    }
+
     return (
         <Modal
             isOpen={modalRemoveWriter}
@@ -24,10 +45,23 @@ export function RemoveWriterModal({
                 <p>
                     {`Tem certeza que deseja remover o jornalista "${selectedWriter.name}"`}
                 </p>
+                {isError && <p style={{ color: "red" }}>{isError}</p>}
             </Modal.BodyText>
             <Modal.Actions>
-                <button>Cancelar</button>
-                <button onClick={() => {}}>Remover</button>
+                <button onClick={() => setModalRemoveWriter((prev) => !prev)}>
+                    Cancelar
+                </button>
+                <button disabled={isLoading} onClick={removeWriterFn}>
+                    {isLoading && (
+                        <Loader direction="row">
+                            <Loader.TextMessage
+                                color="white"
+                                feedbackMessage="Removendo..."
+                            />
+                            <Loader.Icon />
+                        </Loader>
+                    )}
+                </button>
             </Modal.Actions>
         </Modal>
     );
