30690370402b452128207f1c2f38110233ab5b8d
Luan-Coelho-IFBA <20231bsifsa0001@ifba.edu.br>
2025-09-30 16:23:03 -0300

    Update api.ts

--- frontend/src/server/api.ts
+++ frontend/src/server/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const devMode = "http://localhost:3000";
-const production = "http://18.231.94.165:3000";
+const production = "https://projeto-web-pds-bd2.onrender.com";
 
 const api = axios.create({
     baseURL: production,
